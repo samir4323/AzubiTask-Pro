981a163e63dd51297f8779d56ab36f4113380f27
samir4323 <riffisamir75@gmail.com>
2026-03-13 02:25:33 +0000

    Implement Sanctum Authentication, AuthController and API security

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource ("tasks",TaskController::class);
+Route::middleware("auth:sanctum")->group(function(){
+    Route::apiResource ("tasks",TaskController::class);
+    Route::post("/logout",[AuthController::class],"logout");
+});
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
